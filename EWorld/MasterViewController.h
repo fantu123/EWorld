35c78e9007e662142937bbb9eb7c4820817a5d14
mac <mac@macdeMacBook-Pro.local>
2015-04-27 16:42:38 +0800

    Initial Commit

---
+++ EWorld/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  EWorld
+//
+//  Created by mac on 15/4/27.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
